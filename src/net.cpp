585a9ea934d7197cd6c9b9d5ba82a4d9844673df
gladcow <sergey@dash.org>
2018-11-20 23:12:15 +0300

    fix lock requests filtering

--- src/net.cpp
+++ src/net.cpp
@@ -2629,6 +2629,15 @@ void CConnman::RelayTransaction(const CTransaction& tx)
     LOCK(cs_vNodes);
     BOOST_FOREACH(CNode* pnode, vNodes)
     {
+        if (nInv == MSG_TXLOCK_REQUEST) {
+            // Additional filtering for lock requests.
+            // Make it here because lock request processing
+            // differs from simple tx processing in PushInventory
+            // and tx info will not be available there.
+            LOCK(pnode->cs_filter);
+            if(pnode->pfilter && !pnode->pfilter->IsRelevantAndUpdate(tx))
+                continue;
+        }
         pnode->PushInventory(inv);
     }
 }
